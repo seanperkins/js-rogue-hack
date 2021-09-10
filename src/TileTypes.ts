d50006ae4ed0203bb5527546e1f2f401417e1998
Sean Perkins <sean@mobility-labs.com>
2021-09-10 13:05:26 -0400

    Spawn player in last room

---
+++ src/TileTypes.ts
@@ -0,0 +1,48 @@
+import {BLACK, GREEN, LIGHT_GREEN} from './constants/colors'
+
+type GraphicTile = {
+  ch: string
+  fg: string
+  bg: string
+}
+
+type TileData = {
+  walkable: boolean
+  transparent: boolean
+  dark: GraphicTile
+  light: GraphicTile
+}
+
+function newTile(
+  walkable: boolean,
+  transparent: boolean,
+  dark: GraphicTile,
+  light: GraphicTile,
+): TileData {
+  return {
+    walkable,
+    transparent,
+    dark,
+    light,
+  }
+}
+
+export const SHROUD: GraphicTile = {
+  ch: ' ',
+  fg: BLACK,
+  bg: BLACK,
+}
+
+export const floor = newTile(
+  true,
+  true,
+  {ch: '.', fg: GREEN, bg: BLACK},
+  {ch: '.', fg: LIGHT_GREEN, bg: BLACK},
+)
+
+export const wall = newTile(
+  false,
+  false,
+  {ch: '#', fg: GREEN, bg: BLACK},
+  {ch: '#', fg: LIGHT_GREEN, bg: BLACK},
+)
