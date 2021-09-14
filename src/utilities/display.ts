7b44a1d571acbf2d0390e4fcb1331d91f0cffa18
Sean Perkins <sean@mobility-labs.com>
2021-09-13 21:24:37 -0400

    Add windows and fill display

--- src/utilities/display.ts
+++ src/utilities/display.ts
@@ -10,11 +10,16 @@ export function clearDisplay(
   x: number = 0,
   y: number = 0,
 ) {
-  for (let w = 0; w < width; w++) {
-    for (let h = 0; h < height; h++) {
-      display.draw(w + x, h + y, 'X', BLACK, BLACK)
-    }
-  }
+  fillDisplay({
+    display,
+    h: height,
+    w: width,
+    x,
+    y,
+    fg: BLACK,
+    bg: BLACK,
+    char: ' ',
+  })
 }
 
 export function drawFrame(
@@ -48,4 +53,106 @@ export function drawFrame(
     }
   }
   display.drawText(x + 1, y, title)
+  return {
+    w: width - 2,
+    h: height - 2,
+    x: x + 1,
+    y: y + 2,
+  }
+}
+
+export function fillDisplay({
+  display,
+  h,
+  w,
+  x,
+  y,
+  fg,
+  bg,
+  char,
+}: {
+  display: Display
+  h: number
+  w: number
+  x: number
+  y: number
+  fg?: string
+  bg?: string
+  char?: string
+}) {
+  const c = char || ' '
+  const f = fg || LIGHT_GREEN
+  const b = bg || BLACK
+  for (let ww = 0; ww < w; ww++) {
+    for (let hh = 0; hh < h; hh++) {
+      display.draw(ww + x, hh + y, char, f, b)
+    }
+  }
+}
+
+export function drawWindow({
+  display,
+  h,
+  w,
+  x,
+  y,
+  title,
+  fg,
+  bg,
+  shadow,
+}: {
+  display: Display
+  h: number
+  w: number
+  x: number
+  y: number
+  title: string
+  fg?: string
+  bg?: string
+  shadow?: boolean
+}) {
+  const f = fg || LIGHT_GREEN
+  const b = bg || BLACK
+
+  drawFrame(display, h, w, x, y)
+  fillDisplay({
+    display,
+    h: 1,
+    w: w - 2,
+    x: x + 1,
+    y: y + 1,
+    fg: f,
+    bg: f,
+    char: ' ',
+  })
+  display.drawText(x + 1, y + 1, `%c{${b}}%b{${f}}${title}`)
+  if (shadow) {
+    fillDisplay({
+      display,
+      h: 1,
+      w: w,
+      x: h + 1,
+      y: y + h,
+      fg: f,
+      bg: b,
+      char: '°',
+    })
+    fillDisplay({
+      display,
+      h: h - 1,
+      w: 1,
+      x: x + w,
+      y: y + 1,
+      fg: f,
+      bg: b,
+      char: '°',
+    })
+  }
+
+  return {
+    w: w - 2,
+    h: h - 2,
+    x: x + 1,
+    y: y + 2,
+  }
 }
